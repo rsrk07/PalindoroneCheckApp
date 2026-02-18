e8121f5384ffd7c9754ad98693f47ed732e11577
rsrk07 <rsraghavkrishna2007@gmail.com>
2026-02-18 15:53:25 +0530

    raghav Add rsrk07

--- App/src/PalindoroneCheckApp.java
+++ App/src/PalindoroneCheckApp.java
@@ -2,5 +2,6 @@ public class PalindoroneCheckApp {
     public static void main(String[] args){
         System.out.println("welcome to palindrome checker app management system");
         System.out.println("modify this logic to use case 2");
+        System.out.println("system runned succesfully");
     }
 }
